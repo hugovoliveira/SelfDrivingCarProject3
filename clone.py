c81f51800f1b6f9127870ced305fcfe9c4ee602f
hugovoliveira <oliveira.hugo@gmail.com>
2017-04-04 23:46:56 -0300

    single epoch

--- clone.py
+++ clone.py
@@ -183,7 +183,7 @@ validation_generator = generator(validation_samples, batch_size=32)
 
 model.fit_generator(train_generator, samples_per_epoch= len(train_samples), 
                      validation_data=validation_generator, nb_val_samples=len(validation_samples), 
-                     nb_epoch=3)
+                     nb_epoch=1)
         
 
 model.save('model.h5')
