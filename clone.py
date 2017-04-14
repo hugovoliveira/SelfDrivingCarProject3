6402ab3bc23545ad24a02c8a6af709baf4613835
hugovoliveira <oliveira.hugo@gmail.com>
2017-04-13 21:36:13 -0300

    first version that does not always output zero

--- clone.py
+++ clone.py
@@ -23,20 +23,20 @@ from keras.layers.convolutional import Convolution2D, MaxPooling2D, Cropping2D
 
 model = Sequential()
 model.add(Convolution2D(6, 5, 5, input_shape=((160-60-25)*1,320,3)))
-model.add(Activation('sigmoid'))
+model.add(Activation('relu'))
 model.add(MaxPooling2D((2, 2)))
 model.add(Convolution2D(16, 5, 5))
-model.add(Activation('sigmoid'))
+model.add(Activation('relu'))
 model.add(MaxPooling2D((2, 2)))
 model.add(Flatten())
 model.add(Dense(40))
-model.add(Activation('sigmoid'))
+model.add(Activation('relu'))
 model.add(Dropout(0.7))
 model.add(Dense(30))
-model.add(Activation('sigmoid'))
+model.add(Activation('relu'))
 model.add(Dropout(0.7))
 model.add(Dense(1))
-model.add(Activation('sigmoid'))
+model.add(Activation('relu'))
 
 model.compile(loss='mae', optimizer='adam')
 
@@ -116,18 +116,32 @@ with open(os.path.join('.','driving_log.csv')) as csvfile:
                 ten_counter =0
                 
 samples =[]
+steer_abs_sum = 0
+correction = 0.1
 
-correction = 0.2
 
 for line in fileLines:
+    steer_angle = -float(line[3])
+    samples.append([line[0], steer_angle, 'front camera - flip'])
+    steer_abs_sum = steer_abs_sum + abs(steer_angle)
     for i in range(0,3):
         if i==0:
-            samples.append([line[i], float(line[3])])
+            steer_angle = float(line[3])
+            samples.append([line[i], steer_angle, 'front camera'])
+            steer_abs_sum = steer_abs_sum + abs(steer_angle)      
         elif i == 1:
-            samples.append([line[i], float(line[3])+correction])
+            steer_angle = float(line[3])+correction
+            samples.append([line[i], steer_angle, 'left camera'])
+            steer_abs_sum = steer_abs_sum + abs(steer_angle) 
         else: #i==2
-            samples.append([line[i], float(line[3])-correction])
+            steer_angle = float(line[3])-correction
+            samples.append([line[i], steer_angle, 'right camera'])
+            steer_abs_sum = steer_abs_sum + abs(steer_angle) 
 
+
+steer_abs_mean = steer_abs_sum/float(len(samples))
+
+print('Mean steering angle =  ' + str(steer_abs_mean))
 
 from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
@@ -135,7 +149,7 @@ train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 print('Number of initial images: {}'.format(len(train_samples)))
 
 
-def generator(samples, batch_size=200):
+def generator(samples, batch_size=100):
     num_samples = len(samples)
 
     while 1: # Loop forever so the generator never terminates
@@ -148,21 +162,25 @@ def generator(samples, batch_size=200):
                 
                 angle = batch_a_sample[1]
                 path_split = batch_a_sample[0].split('/')
-                converted_patch = os.path.join('.',*path_split)
+                converted_path = os.path.join('.',*path_split)
 
-                image = cv2.imread(converted_patch, cv2.IMREAD_COLOR)[60:135,:,:]
+                image = cv2.imread(converted_path, cv2.IMREAD_COLOR)[60:135,:,:]
+                if batch_a_sample[2] == 'front camera - flip':
+                    image = cv2.flip(image,1)                    
 
                 timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
-#                 cv2.imwrite('Strgt-ang {:.2f} - {}.jpg'.format(angle, timestamp), image)             
+                marked_image = image.copy()
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(marked_image, batch_a_sample[2] + 'Strng:' + str(angle),(20,60), font, 0.5,(255,255,255),1,cv2.LINE_AA)
+#                 cv2.imwrite(timestamp +'.jpg', marked_image)            
                 image = image/255.0 -0.5
                 image_vec.append(image)
                 steerinc_vec.append(angle)
-                image = cv2.flip(image,1)
-                image_vec.append(image)
-                steerinc_vec.append(-angle)
+
 
             X_train = np.array(image_vec)
             y_train = np.array(steerinc_vec)
+#             print(len(y_train))
 #             print(y_train)
 #             print(len((X_train, y_train)),  flush=True)
             yield sklearn.utils.shuffle(X_train, y_train)
@@ -176,9 +194,9 @@ validation_generator = generator(validation_samples, batch_size=20)
 
 
 
-model.fit_generator(train_generator, samples_per_epoch= len(train_samples)*2, 
-                     validation_data=validation_generator, nb_val_samples=len(validation_samples)*2, 
-                     nb_epoch=8)
+model.fit_generator(train_generator, samples_per_epoch= len(train_samples), 
+                     validation_data=validation_generator, nb_val_samples=len(validation_samples), 
+                     nb_epoch=16)
         
 
 
@@ -188,5 +206,6 @@ model.save('model.h5')
 for j in range(500,550):
     image = cv2.imread(fileLines[j][0])[25:100,:,:]
     image_array = np.asarray([image])
+    image_array = image_array/255.0 - 0.5
     steering = model.predict(image_array, batch_size=1)
     print('Prediction: ' +str(steering))
